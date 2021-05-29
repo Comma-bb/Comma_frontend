5f63bfb53e60f04e127a4d86a445a8a2a92c094d
daeun <sung980524@naver.com>
2021-05-29 18:11:02 +0900

    같은거 insert 안되게

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 var systemDB;
 var dataset;
+let isTrue = true;
 
 init();
 
@@ -34,6 +35,7 @@ function init(){
                 
     createTable(db);
     systemDB = db; 
+
     setData();
 }
 
@@ -57,16 +59,56 @@ function createTable(db) {
     });
 }
 
-function insertDB(db, title, pubDate, genre, director, actor, img, color1, color2, color3, color4, color5,url) {
-    var strSql = "INSERT INTO movie_board (title, pubDate, genre, director, actor, img, color1, color2, color3, color4, color5, url) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)"; 
+function checkDB(db,title){
+    isTrue = true;
+    var check = "select 1 from movie_board WHERE title = ?";
+    console.log("checkDB 들어옴");
 
+    //같은 이름 존재 하는 경우 어떻게 알지 
     db.transaction(function (tx) { 
-        tx.executeSql(strSql, [title, pubDate, genre, director, actor, img, color1, color2, color3, color4, color5,url]);
-    });
+        tx.executeSql(check, [title], function (tx, result) { 
+            dataset = result.rows;
+            console.log(dataset);
+            if(dataset.length>0){
+                console.log("같은 이름 존재");
+                // dropTable(db);
+                isTrue = false;
+            }
+            else{
+                console.log("같은 이름 없음");
+                isTrue = true;
+            }
+        }); 
+    }); 
+}
+
+function insertDB(db, title, pubDate, genre, director, actor, img, color1, color2, color3, color4, color5,url) {
+    
+    //DB에 title 존재하는지 확인 
+    //isTrue = true
+    checkDB(db,title);
+
+    setTimeout(function(){
+        if(isTrue){
+            console.log("같은 이름 존재하지 않음");
+        }
+        else{
+            console.log("같은 이름 존재 하는 경우");
+            console.log("insert  취소");
+            return;
+        }
+        
+        var strSql = "INSERT INTO movie_board (title, pubDate, genre, director, actor, img, color1, color2, color3, color4, color5, url) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)"; 
+    
+        db.transaction(function (tx) { 
+            tx.executeSql(strSql, [title, pubDate, genre, director, actor, img, color1, color2, color3, color4, color5,url]);
+        });
+    },100);
 }
 
 //임시로 idx로 영화제목만 가져오기 -> 나중에 컬럼 한번에 가져올꺼임!~!
 function getTitle(){
+    var title;
     console.log("getTitle 들어옴");
     var strSql = "SELECT title FROM movie_board WHERE idx = ?"; 
     systemDB.transaction(function (tx) { 
@@ -74,9 +116,48 @@ function getTitle(){
             dataset = result.rows;
             if(dataset.length > 0){
                 item = dataset.item(0);
-                console.log(item['title']);
-                return item['title'];
-            }      
+                title = item['title'];
+                console.log(title);
+
+                return String(title);
+                // $("#title").text(title);
+            }     
         });
     });
+}
+
+function selectAllList() { 
+    db = systemDB
+    var strSql = "SELECT * FROM movie_board"; 
+    db.transaction(function (tx) { 
+        tx.executeSql(strSql, [], function (tx, result) { 
+            dataset = result.rows; 
+            var str = "<ol>";
+            if (dataset.length > 0) {
+                for (var i = 0, item = null; i < dataset.length; i++) { 
+                    item = dataset.item(i); 
+                    str += "<li>"+item['title']+ "</li>";                 
+                }
+            } else {
+                str += "리스트가 없습니다.";
+            }
+            str += "</ol>";  
+            $("#title").html(str);
+        }); 
+    }); 
+}
+
+// 테이블 삭제
+function dropTable(db) { 
+    if (!confirm("테이블 삭제 하시겠습니까?")) {
+        return;    
+    }
+    
+    var strSql = "DROP TABLE movie_board"; 
+    db.transaction(function (tx) { 
+        tx.executeSql(strSql, []); 
+    }); 
+    console.log("dropTable");
+    setTimeout(selectAllList,1000);
+    // selectAllList();
 }
--- js/main-script.js
+++ js/main-script.js
@@ -21,22 +21,22 @@ $(document).ready(function () {
 
     //owl carousel settings
 
-    $(".owl-carousel").owlCarousel({
-        loop: false,
-        margin: 10,
-        nav: true,
-        responsive: {
-            0: {
-                items: 2
-            },
-            600: {
-                items: 2
-            },
-            1000: {
-                items: 5
-            }
-        }
-    });
+    // $(".owl-carousel").owlCarousel({
+    //     loop: false,
+    //     margin: 10,
+    //     nav: true,
+    //     responsive: {
+    //         0: {
+    //             items: 2
+    //         },
+    //         600: {
+    //             items: 2
+    //         },
+    //         1000: {
+    //             items: 5
+    //         }
+    //     }
+    // });
 
     //autoplay video on hover over an video item
     var figure = $(".video").hover(hoverVideo, hideVideo);
